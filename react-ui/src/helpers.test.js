a92a316a2febb7956d3f38ea338790ed37dac8f2
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-07-19 00:55:17 -0700

    setup testing functions

--- react-ui/src/helpers.test.js
+++ react-ui/src/helpers.test.js
@@ -1,4 +1,16 @@
+import * as func from './helpers';
+
 it('loads helper test file', () => {
   expect(true).toBe(true);
+});
+
+//end to end tests
+it('should use an exported function', () => {
+  expect(func.isHigherHand()).toBe('test string');
+});
+
+//helper function tests
+it('should use an exported function', () => {
+  expect(func.cardMapforComparison({rank: 2, suit: 0})).toEqual({rank: 15, suit: 11});
 });
 
