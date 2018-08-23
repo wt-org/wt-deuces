a628dede818e18b5278f5a438aa130231c4f9612
anthonyau <anthony.tk.au@gmail.com>
2018-08-22 21:46:17 -0700

    game logic yo

--- react-ui/src/helpers.js
+++ react-ui/src/helpers.js
@@ -1,8 +1,19 @@
-//example hand:
-let testHand = [{rank: 2, suit: 0},{rank: 2, suit: 1},{rank: 2, suit: 2},{rank: 2, suit: 3}];
 //suit rules from imported library:    0=clubs, 1=diamonds, 2=hearts, 3=spades
 //map to these rules:   diamonds=10, clubs=11, hearts=12, spades=13
 //rank rules:  1-13, but map 1(ace)=14 and 2(deuce)=15
+
+//ranking of 5 five card hands
+
+// invalid hand = 0
+// single = 1
+// pair = 2
+// straight = 3
+// flush = 4
+// full house = 5
+// four of a kind = 6
+// straight flush = 7
+
+//add alerts
 
 const SUIT_MAP = {
   0: 11,
@@ -28,38 +39,216 @@ export function handMapforComparison(hand) {
   return hand.map(card => cardMapforComparison(card))
 }
 
-//this is the final function exported and used in the app to compare hands
-//input = 2 hands, output = boolean (true if playerHand trumps tableHand, false if playerHand doesn't beat tableHand)
-export function isHigherHand(tableHand, playerHand) {
+export function isHigherHand(tableHand, playerHand) { //FINAL ONE
   let mappedTableHand = handMapforComparison(tableHand);
   let mappedPlayerHand = handMapforComparison(playerHand);
-  //check that hands are the same amount of cards
-  //if one-card hand
-    //compare that card
-  //if 2-card hand
-    //check that both cards in hand are the same rank
-    //compare highest player card to highest table card
-  //if 5-card hand
-    //check what kind of 5 card hand
-    //compare each 5 card hand
-  return true
+
+  let result = false
+  let tableHandValue = false
+  let playerHandValue = false
+
+  if (isSingle(mappedPlayerHand) === 0) {
+    if (isPair(mappedPlayerHand) === 0) {
+      if (isFiveCardHand(mappedPlayerHand) === 0) {
+        playerHandValue = 0
+      } else {
+        playerHandValue = isFiveCardHand(mappedPlayerHand)
+      }
+    } else {
+      playerHandValue = isPair(mappedPlayerHand)
+    }
+  } else {
+    playerHandValue = isSingle(mappedPlayerHand)
+  }
+
+  //gives 0 if it's an invalid hand, otherwise it'll give the value of the hands
+
+  if (isSingle(mappedTableHand) === 0) {
+    if (isPair(mappedTableHand) === 0) {
+      if (isFiveCardHand(mappedTableHand) === 0) {
+        tableHandValue = 0
+      } else {
+        tableHandValue = isFiveCardHand(mappedTableHand)
+      }
+    } else {
+      tableHandValue = isPair(mappedTableHand)
+    }
+  } else {
+    tableHandValue = isSingle(mappedTableHand)
+  }
+
+  if (tableHandValue === playerHandValue) {
+    let highestCardinPlayerHand = highestCardandRank(mappedPlayerHand)
+    let highestCardinTableHand = highestCardandRank(mappedTableHand)
+    //check for flush
+    if (tableHandValue === 4) {
+      if (highestCardinPlayerHand.highestSuit > highestCardinTableHand.highestSuit) {
+        result = true
+      } else if (highestCardinPlayerHand.highestSuit === highestCardinTableHand.highestSuit) {
+        if (highestCardinPlayerHand.highestRank > highestCardinTableHand.highestRank) {
+          result = true
+        }
+      }
+    } else { //check for everything else
+      if (highestCardinPlayerHand.highestRank > highestCardinTableHand.highestRank) {
+        result = true
+      } else if (highestCardinPlayerHand.highestRank === highestCardinTableHand.highestRank) {
+        if (highestCardinPlayerHand.highestSuit > highestCardinTableHand.highestSuit) {
+          result = true
+        }
+      }
+    }
+
+  }
+  return result
 }
 
-export function isSameAmount(hand1, hand2) {
-  //TODO: must be 1, 2, or 5
-  return hand1.length === hand2.length;
+
+export function isSingle(hand) {
+  let result = 0
+  if (hand.length === 1) {
+    result = 1
+  } 
+  return result
 }
 
-function compareSingleCard(card1, card2) {
-  //compare rank (if same rank, then compare suit)
+export function isPair(hand) {
+  let result = 0
+  if (hand.length === 2 && (hand[0].rank === hand[1].rank)) {
+    result = 2
+  }
+  return result
 }
 
-function areCardValuesEqual(card1, card2) {
-  return card1.rank === card2.rank;
+export function isFiveCardHand(hand) {
+  let result = 0
+  let rankOfAllCards = []
+  let suitOfAllCards = new Set()
+  if (hand.length === 5) {
+    for (var i = 0; i < hand.length; i++) {
+      rankOfAllCards.push(hand[i].rank)
+      suitOfAllCards.add(hand[i].suit)
+    }
+    rankOfAllCards.sort(function(a, b){return a - b})
+
+    let straight = false
+    let flush = false
+    let full_house = false
+    let four_kind = false      
+
+    //check if straight
+    let tempStraight = true
+    for (var y = 0; y < rankOfAllCards.length - 1; y++) {
+      let difference = rankOfAllCards[y+1] - rankOfAllCards[y]
+      if (difference !== 1) {
+        tempStraight = false
+      }
+    }
+    if (tempStraight === false) {
+      straight = false 
+    } else { 
+      straight = true
+    }
+
+    //check if flush
+    if (suitOfAllCards.size === 1) {
+      flush = true
+    }
+
+    //check if four of a kind or full house
+    var countOfCards = {}
+    for (var w = 0; w < rankOfAllCards.length; w++) {
+      if (countOfCards.hasOwnProperty(rankOfAllCards[w].toString())) {
+        countOfCards[rankOfAllCards[w]]++
+      } else {
+        countOfCards[rankOfAllCards[w]] = 1
+      }
+    }
+
+    let tempFourKind = false
+    let tempOneForFourKind = false
+    let tempPair = false
+    let tempThreeKind = false
+    for (var key in countOfCards) {
+      if (countOfCards[key] === 3) {
+        tempThreeKind = true
+      }
+      if (countOfCards[key] === 2) {
+        tempPair = true
+      }
+      if (countOfCards[key] === 4) {
+        tempFourKind = true
+      }
+      if (countOfCards[key] === 1) {
+        tempOneForFourKind = true
+      }
+    }
+
+    if (tempThreeKind === true && tempPair === true) {
+      full_house = true
+    }
+
+    if (tempFourKind === true && tempOneForFourKind === true) {
+      four_kind = true
+    }
+
+    let lowStraightAce = [3,4,5,14,15]
+    let lowStraightDeuce = [3,4,5,6,15]
+
+    
+    if (JSON.stringify(lowStraightAce) === JSON.stringify(rankOfAllCards)) {
+      straight = true
+    }
+
+    if (JSON.stringify(lowStraightDeuce) === JSON.stringify(rankOfAllCards)) {
+      straight = true
+    }
+
+    if (straight === true) {
+      result = 3
+    }
+    if (flush === true) {
+      result = 4
+    }
+
+    if (full_house === true){
+      result = 5
+    }
+
+    if (four_kind === true){
+      result = 6
+    }
+
+    if (straight === true && flush === true){
+      result = 7
+    }
+  }
+  return result
 }
 
-function returnHighestCard() {}
+export function highestCardandRank(hand) {
+  let highestRank = 0
+  let highestSuit = 0
+  for (var i = 0; i < hand.length; i++) {
+    let rank = hand[i].rank
+    let suit = hand[i].suit
+    if (rank > highestRank) {
+      highestRank = rank
+      highestSuit = suit
+    }
+    if (rank === highestRank) {
+      if (suit > highestSuit) {
+        highestRank = rank
+      highestSuit = suit
+      }
+    }
+  }
+  return {highestRank: highestRank, highestSuit: highestSuit}
+}
 
+export function highestCardandRankFlush(hand) {//not done
+  
+}
 
 
 
--- react-ui/src/helpers.test.js
+++ react-ui/src/helpers.test.js
@@ -1,16 +1,95 @@
 import * as func from './helpers';
 
-it('loads helper test file', () => {
-  expect(true).toBe(true);
-});
+// straight = 3
+// flush = 4
+// full house = 5
+// four of a kind = 6
+// straight flush = 7
 
-//end to end tests
-it('should use an exported function', () => {
-  expect(func.isHigherHand()).toBe('test string 1');
-});
+//valid hands
+let singleValidAce = [{rank: 14, suit: 0}];
+let pairValidAces = [{rank: 14, suit: 0}, {rank: 14, suit: 1}];
+let invalidPair = [{rank: 14, suit: 0}, {rank: 12, suit: 1}];
+let validPair = [{rank: 6, suit: 3}, {rank: 6, suit: 2}];
+
+let validStraight = [{rank: 14, suit: 0}, {rank: 12, suit: 1}, {rank: 13, suit: 3}, {rank: 10, suit: 3}, {rank: 11, suit: 1}];
+let validStraight2 = [{rank: 6, suit: 2}, {rank: 8, suit: 1}, {rank: 7, suit: 3}, {rank: 5, suit: 3}, {rank: 4, suit: 1}];
+let validStraightLowAce = [{rank: 14, suit: 3}, {rank: 15, suit: 2}, {rank: 3, suit: 1}, {rank: 4, suit: 0}, {rank: 5, suit: 3}];
+let validStraightLowDeuce = [{rank: 3, suit: 3}, {rank: 4, suit: 2}, {rank: 5, suit: 1}, {rank: 6, suit: 0}, {rank: 15, suit: 3}];
+
+let validFlush = [{rank: 6, suit: 3}, {rank: 8, suit: 3}, {rank: 10, suit: 3}, {rank: 5, suit: 3}, {rank: 4, suit: 3}];
+
+let validFullHouse = [{rank: 6, suit: 3}, {rank: 6, suit: 2}, {rank: 6, suit: 1}, {rank: 5, suit: 2}, {rank: 5, suit: 3}];
+let validFullHouse2 = [{rank: 3, suit: 3}, {rank: 3, suit: 2}, {rank: 7, suit: 1}, {rank: 7, suit: 2}, {rank: 7, suit: 3}];
+
+
+let validFourKind = [{rank: 6, suit: 3}, {rank: 6, suit: 2}, {rank: 6, suit: 1}, {rank: 6, suit: 0}, {rank: 5, suit: 3}];
+
+let validStraightFlush = [{rank: 10, suit: 0}, {rank: 11, suit: 0}, {rank: 12, suit: 0}, {rank: 13, suit: 0}, {rank: 14, suit: 0}];
+let validStraightFlushSpades = [{rank: 10, suit: 3}, {rank: 11, suit: 3}, {rank: 12, suit: 3}, {rank: 13, suit: 3}, {rank: 14, suit: 3}];
+let validLowDeuceStraightFlush = [{rank: 4, suit: 3}, {rank: 3, suit: 3}, {rank: 15, suit: 3}, {rank: 5, suit: 3}, {rank: 6, suit: 3}];
+
+let invalidFiveCardHand = [{rank: 3, suit: 3}, {rank: 8, suit: 2}, {rank: 10, suit: 1}, {rank: 6, suit: 12}, {rank: 9, suit: 3}];
+
+
 
 //helper function tests
-it('should return true or false', () => {
+it('should return rank 15 and suit 11', () => {
   expect(func.cardMapforComparison({rank: 2, suit: 0})).toEqual({rank: 15, suit: 11});
 });
+
+it('valid single should return "true"', () => {
+  expect(func.isSingle(singleValidAce)).toEqual(1);
+});
+
+it('invalid single should return "false"', () => {
+  expect(func.isSingle(pairValidAces)).toEqual(0);
+});
+
+it('valid pair should return "true"', () => {
+  expect(func.isPair(pairValidAces)).toEqual(2);	
+});
+
+it('invalid pair should return "false"', () => {
+  expect(func.isPair(singleValidAce)).toEqual(0);
+  expect(func.isPair(invalidPair)).toEqual(0);
+});
+
+it('should return highest rank and suit', () => {
+  expect(func.highestCardandRank(singleValidAce)).toEqual({highestRank: 14, highestSuit: 0});
+  expect(func.highestCardandRank(pairValidAces)).toEqual({highestRank: 14, highestSuit: 1});
+  expect(func.highestCardandRank(validStraight)).toEqual({highestRank: 14, highestSuit: 0});
+  expect(func.highestCardandRank(validStraight2)).toEqual({highestRank: 8, highestSuit: 1});
+  expect(func.highestCardandRank(validFullHouse)).toEqual({highestRank: 6, highestSuit: 3});
+});
+
+it('should return value of hand', () => {
+  expect(func.isFiveCardHand(singleValidAce)).toEqual(0);
+  expect(func.isFiveCardHand(pairValidAces)).toEqual(0);
+  expect(func.isFiveCardHand(validStraight)).toEqual(3);
+  expect(func.isFiveCardHand(validStraight2)).toEqual(3);
+  expect(func.isFiveCardHand(validFlush)).toEqual(4);
+  expect(func.isFiveCardHand(validFourKind)).toEqual(6);
+  expect(func.isFiveCardHand(validFullHouse)).toEqual(5);
+  expect(func.isFiveCardHand(validFullHouse2)).toEqual(5);
+  expect(func.isFiveCardHand(validStraightFlush)).toEqual(7);
+  expect(func.isFiveCardHand(validLowDeuceStraightFlush)).toEqual(7);
+});
+
+it('should return highest hand', () => {
+  expect(func.isHigherHand(validPair, pairValidAces)).toEqual(true);
+  expect(func.isHigherHand(pairValidAces, validPair)).toEqual(false);
+  expect(func.isHigherHand(validPair, validPair)).toEqual(false);
+  expect(func.isHigherHand(validPair, singleValidAce)).toEqual(false);
+  expect(func.isHigherHand(validPair, validStraightFlush)).toEqual(false);
+  expect(func.isHigherHand(validStraightFlush, validStraightFlushSpades)).toEqual(true);
+  expect(func.isHigherHand(validStraightFlushSpades, validStraightFlush)).toEqual(false);
+  expect(func.isHigherHand(validFullHouse2, validFullHouse)).toEqual(false);
+  expect(func.isHigherHand(validFullHouse, validFullHouse2)).toEqual(true);	
+  expect(func.isHigherHand(validStraightFlush, validFourKind)).toEqual(false);
+  expect(func.isHigherHand(validFourKind, validStraightFlush)).toEqual(false);
+  expect(func.isHigherHand(validFourKind, invalidFiveCardHand)).toEqual(false);
+});
+
+
 
